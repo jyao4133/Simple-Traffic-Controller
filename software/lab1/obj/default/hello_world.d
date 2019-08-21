22f7f5693c1274fe5feb2d35801f260faf8b3cde
Jacob Allen <35180619+jacoballen4534@users.noreply.github.com>
2019-08-21 20:47:46 +1200

    Sorta working q3, working q2

---
+++ software/lab1/obj/default/hello_world.d
@@ -0,0 +1,24 @@
+obj/default/hello_world.o: hello_world.c \
+ ../lab1_bsp//HAL/inc/sys/alt_alarm.h \
+ ../lab1_bsp//HAL/inc/sys/alt_llist.h ../lab1_bsp//HAL/inc/alt_types.h \
+ ../lab1_bsp//HAL/inc/priv/alt_alarm.h ../lab1_bsp/system.h \
+ ../lab1_bsp/linker.h ../lab1_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../lab1_bsp//HAL/inc/io.h ../lab1_bsp//HAL/inc/alt_types.h
+
+../lab1_bsp//HAL/inc/sys/alt_alarm.h:
+
+../lab1_bsp//HAL/inc/sys/alt_llist.h:
+
+../lab1_bsp//HAL/inc/alt_types.h:
+
+../lab1_bsp//HAL/inc/priv/alt_alarm.h:
+
+../lab1_bsp/system.h:
+
+../lab1_bsp/linker.h:
+
+../lab1_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../lab1_bsp//HAL/inc/io.h:
+
+../lab1_bsp//HAL/inc/alt_types.h:
